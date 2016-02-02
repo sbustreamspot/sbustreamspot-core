73a7646bc95376949f4734ed44e21602ca9f6941
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2016-02-02 15:37:38 -0500

    fix directories

--- experiments/simulate_perfect_lsh_clustering.py
+++ experiments/simulate_perfect_lsh_clustering.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 
-import math
-import networkx as nx
 import sys
+import networkx as nx
 
 ATTACK_GIDS = range(300,400)
 NORMAL_GIDS = range(0,300) + range(400,600)
 
-b = float(sys.argv[1])
-r = float(sys.argv[2])
-threshold = (1/b) ** (1/r) 
+threshold = float(sys.argv[1])
 
 # Union-find from Kruskal's algorithm:
 # https://github.com/israelst/Algorithms-Book--Python/blob/master/5-Greedy-algorithms/kruskal.py
@@ -36,8 +33,8 @@ def union(vertice1, vertice2):
             if rank[root1] == rank[root2]: rank[root2] += 1
 # End.
 
-for C in [25, 50, 100]:
-    print 'Clusters for C:', C, 'Threshold:', threshold
+for C in [25, 50, 100, 150, 200, 300, 500]:
+    print 'Clusters for C:', C
     dists = [[0 for i in range(600)] for j in range(600)]
     with open('sim_results_C' + str(C) + '.txt', 'r') as f:
         next(f)
@@ -48,25 +45,14 @@ for C in [25, 50, 100]:
             fields = line.strip().split('\t')
             g1 = int(fields[0])
             g2 = int(fields[1])
-            sim = float(fields[2]) # cosine similarity = cos(theta)
-            dists[g1][g2] = 1 - math.acos(sim)/math.pi
-            dists[g2][g1] = 1 - math.acos(sim)/math.pi
+            sim = float(fields[2])
+            dists[g1][g2] = 1.0 - sim
+            dists[g2][g1] = 1.0 - sim    
 
     G = nx.Graph()
-    for v1 in NORMAL_GIDS:
-        for v2 in NORMAL_GIDS:
-            if v1 == v2:
-                continue
-
-            if dists[v1][v2] >= threshold:
-                G.add_edge(v1, v2)
+    for v1, v2 in zip(NORMAL_GIDS, NORMAL_GIDS):
+        if sim[v1][v2] >= threshold:
+            G.add_edge(v1, v2)
 
     for i, cc in enumerate(nx.connected_components(G)):
         print 'CC ' + str(i) + ': ' + str(cc)
-        cluster_anomalies = []
-        for a in ATTACK_GIDS:
-            for v in cc:
-                if dists[a][v] >= threshold:
-                    cluster_anomalies.append((a, dists[a][v]))
-        if len(cluster_anomalies) > 0:
-            print '\tANOMALIES:', cluster_anomalies
